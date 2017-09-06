0146c5e1832a44e18b96463de2b0f22c9a299f93
tigranavetis <tigranavetisyan94@gmail.com>
2017-09-06 11:40:44 +0400

    Add content file fow architect course

---
+++ content.js
@@ -0,0 +1,111 @@
+module.exports = [
+	{
+		title: '1.Types_of_Architecture',
+		subFolders: [
+			{
+				title: '1.Application Architecture',
+				entryId: '0_0h0wjw6m',
+				flavorId: '0_js31g7p9',
+				number: 174,
+			},
+			{
+				title: '2.Integration Architecture',
+				entryId: '0_uwm0dmuo',
+				flavorId: '0_qtxkjrro',
+				number: 288,
+			},
+			{
+				title: '3.Enterprise Architecture',
+				entryId: '0_o5oyhlg7',
+				flavorId: '0_09cbw3qg',
+				number: 91,
+			}
+		]
+	},
+	{
+		title: '2.Architectural_Patterns',
+		subFolders: [
+			{
+				title: '1.Architectural Patterns',
+				entryId: '0_m32n64s8',
+				flavorId: '0_ia3iz0kl',
+				number: 291,
+			},
+			{
+				title: '2.Microkernel Architecture',
+				entryId: '0_zyiip27l',
+				flavorId: '0_9o7bmgoa',
+				number: 215,
+			}
+		]
+	},
+	{
+		title: '3.Pitfalls',
+		subFolders: [
+			{
+				title: '1.Architecture Anti-Patterns, Part 1',
+				entryId: '0_giev8e5e',
+				flavorId: '0_kz7x68qw',
+				number: 96,
+			},
+			{
+				title: '2.Architecture Anti-Patterns, Part 2',
+				entryId: '0_ibhiwk45',
+				flavorId: '0_pew6ara6',
+				number: 166,
+			},
+			{
+				title: '3.Tooling and Documentation',
+				entryId: '0_iro2sdke',
+				flavorId: '0_nvsjd31m',
+				number: 210,
+			}
+		]
+	},
+	{
+		title: '4.Soft_Skills',
+		subFolders: [
+			{
+				title: '1.Architectural Decisions',
+				entryId: '0_43z8s3dx',
+				flavorId: '0_kjwytota',
+				number: 147,
+			},
+			{
+				title: '2.Architecture Refactoring',
+				entryId: '0_4k5idqmh',
+				flavorId: '0_psre0pcu',
+				number: 155,
+			},
+			{
+				title: '3.Meeting Hacks',
+				entryId: '0_jjvxyokv',
+				flavorId: '0_0m8wtocf',
+				number: 141,
+			}
+		]
+	},
+	{
+		title: '5.Continuous_Delivery',
+		subFolders: [
+			{
+				title: '1.Continuous Delivery Defined',
+				entryId: '0_edav6ny7',
+				flavorId: '0_d8p5h0zi',
+				number: 250,
+			},
+			{
+				title: '2.Deployment Pipelines',
+				entryId: '0_0srtefqg',
+				flavorId: '0_k4jhyuzy',
+				number: 141,
+			},
+			{
+				title: '3.DevOps',
+				entryId: '0_ynouowgj',
+				flavorId: '0_0se3wzge',
+				number: 181,
+			}
+		]
+	},
+];
